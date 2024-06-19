71e9a18e5f5535af777f0a8daecd3cc86dd5ae03
Scrubzie <Scrubzzilla@gmail.com>
2024-06-19 15:05:13 +0800

    Planning validate_runsheet

--- main.py
+++ main.py
@@ -4,14 +4,19 @@ import pandas as pd
 #TODO Input Validation
 #TODO Proper Params
 
-RUNSHEET = ["Woolworths","Coles","Spud Shed"]
-
 LOWER_K = 0
 UPPER_K = 10 #TODO This should be equal to samples in data
 
+mydataset = {
+  'ID': [1, np.nan, 3],
+  'Customer': ["Woolworths Riverton", "Coles Karawara", "Spud Shed Jandakot"]
+}
+
+runsheet = pd.DataFrame(mydataset)
+
 #TODO Try Catch Statement
 def main(runsheet, k):
-    customer_array = validate_runsheet(runsheet)
+    validate_runsheet(runsheet)
 
     validate_k(k)
     #Validate Data
@@ -25,20 +30,26 @@ def validate_k(k):
         raise ValueError(f'Invalid k: must be between {LOWER_K} and {UPPER_K}. k = ({k})')
 
 #TODO Rename
+# Dimension Check <>
+# Label check?
+# Check for Null entries
+# Unique IDs
+# Duplicate Check
+# Format Check???
+# Exists in Db
+# Return a k-means ready distance matrix
+# Need to keep track of which long/lat is for which entry (index-based)
 def validate_runsheet(runsheet):
-    print("Not Implemented Yet")
-    print(type(runsheet))
-    return np.array(runsheet)
+    if runsheet.isnull().values.any():
+        print("Null")
+    else:
+        print("No Nulls")
+    print(runsheet.shape[0]) # Rows
+    print(runsheet.shape[1]) # Columns
     # Right Format
     # Find Long and Lat per entry
 
-main(RUNSHEET,5)
+def __validate_runsheet_ID():
+    print("Not Implemented Yet")
 
-mydataset = {
-  'cars': ["BMW", "Volvo", "Ford"],
-  'passings': [3, 7, 2]
-}
-
-myvar = pd.DataFrame(mydataset)
-
-print(myvar)
+main(runsheet,5)
